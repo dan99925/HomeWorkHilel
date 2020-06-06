a7a2bdc2b0f5c2a439bffc0d000717ba45dbb725
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-06-06 14:06:49 +0300

    Home work for HILLEL Fix

--- HomeWork/HW2.py
+++ HomeWork/HW2.py
@@ -1,4 +1,4 @@
 
-print('*****\t \t *\t\t **\t\t *\n*\t *      * *\t\t * *\t *\n*\t  *\t   *   *\t *  *\t *')
+print('* * *\t \t *\t\t **\t\t *\n*\t *      * *\t\t * *\t *\n*\t  *\t   *   *\t *  *\t *')
 print('*\t  *\t  *\t\t*\t *\t *\t *\n*\t  *  * * * * *\t *\t  *  *\n*\t *\t*\t\t  *  *\t   * * ')
-print('*****  *\t\t   * *\t\t**')
+print('* * *  *\t\t   * *\t\t**')
