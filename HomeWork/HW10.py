8700fa3eb00d87130dc001e1887e2e68c43a601f
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-06-17 18:58:40 +0300

    Home work for HILLEL update

--- HomeWork/HW10.py
+++ HomeWork/HW10.py
@@ -1,10 +1,3 @@
-#
-# s = input('write some txt ')
-# a = [s.find('h') + 1]
-# b = [s.rfind('h') - 1]
-# c = (a + b).replace("h", "H")
-# d = a + b + c
-# print(d)
 s = input('write some text')
 a = s[:s.find('h') + 1]
 b = s[s.find('h') + 1:s.rfind('h')]
