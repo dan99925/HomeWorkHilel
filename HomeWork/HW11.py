3fa59f3ccb12613c5f65097bf37513046fc1d719
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-06-21 18:28:10 +0300

    Home work for HILLEL update

--- HomeWork/HW11.py
+++ HomeWork/HW11.py
@@ -1,46 +1,42 @@
-#
-# size = int(input("number"))
-# for i in range(size):
-#     for j in range(size):
-#         if i == size // 2 or i == size // 2 - j or j == i + size // 2:
-#             print('*  ', end ='')
-#         else:
-#             print('   ', end='')
-#     print()
 
 size = int(input("number"))
-for i in range(size):
+for i in range(size // 2 + 1):
     for j in range(size):
         if i == size // 2 or i == size // 2 - j or j == i + size // 2:
             print('*  ', end ='')
         else:
             print('   ', end='')
     print()
-"""
-i > j >= size - i tringle down:
-i > j <= size - i tringle left:
+print()
 
-"""
+for i in range(size):
+    for j in range(size):
+        if i < size // 2 + 1 and size // 2 - i <= j <= i + size // 2:
+            print('*  ', end ='')
+        else:
+            print('   ', end='')
+    print()
 
+print()
 
+for i in range(size):
+    for j in range(size):
+        if i < size // 2 + 1 and size // 2 - i <= j <= i + size // 2:
+            print('*  ', end='')
+        elif i > size // 2 and (j == size - i - 1 + size // 2 or j == size // 2 - (size - i -1)):
+            print('*  ', end ='')
+        else:
+            print('   ', end='')
+    print()
 
-# size = int(input("number"))
-# for i in range(size):
-#     for j in range(size):
-#         if i == size // 2 or i == size // 2 - j or j == i + size // 2 or i == size // 2 + j or j == size // 2 - i + size:
-#             print('*  ', end ='')
-#         else:
-#             print('   ', end='')
-#     print()
+print()
 
-
-
-
-# size = int(input("number"))
-# for i in range(size // 2 + 1):
-#     for j in range(size):
-#         if i == 0 or i == j or j == size - i - 1 or i < j < size - i:
-#             print('*  ', end ='')
-#         else:
-#             print('   ', end='')
-#     print()
+for i in range(size):
+    for j in range(size):
+        if i < size // 2 + 1 and size // 2 - i <= j <= i + size // 2:
+            print('*  ', end='')
+        elif i > size // 2 and (j == size - i - 1 + size // 2 or j == size // 2 - (size - i -1)) or j == size // 2:
+            print('*  ', end ='')
+        else:
+            print('   ', end='')
+    print()
