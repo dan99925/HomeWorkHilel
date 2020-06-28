3031593401694021d2dd8beef898e3e75c4e4e37
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-06-28 21:28:04 +0300

    Home work for HILLEL update

--- HomeWork/HW14.py
+++ HomeWork/HW14.py
@@ -1,3 +1,4 @@
 import random
 
-print(len(set([random.randint(10, 30) for _ in range(10)])), len(set([random.randint(10, 50) for _ in range(20)])))
+print(len(set([random.randint(10, 30) for _ in range(10)]) & set([random.randint(10, 50) for _ in range(20)])))
+print()
