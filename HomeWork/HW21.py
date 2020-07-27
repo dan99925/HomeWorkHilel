7c6fc4fcdb780e51d16f4aadd7063d7990ddad6d
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-07-27 22:02:54 +0300

    Home work for HILLEL

--- HomeWork/HW21.py
+++ HomeWork/HW21.py
@@ -1,7 +1,14 @@
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in range(len(lst) // 2):
-    a = lst[i]
-    lst[i] = lst[len(lst) - i - 1]
-    lst[len(lst) - i - 1] = a
-print(lst)
+
+
+def lst_revers(x):
+    for i in range(len(lst) // 2):
+        a = lst[i]
+        lst[i] = lst[len(lst) - i - 1]
+        lst[len(lst) - i - 1] = a
+    return x
+
+
+print(lst_revers(lst))
+# lst1 = [int(item) for item in input("Enter the list items : ").split()]
