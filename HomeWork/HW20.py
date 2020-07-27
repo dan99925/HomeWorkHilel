fb2f57ec004e3a79e8e654127480a7cad44a21ff
dan99925 <63469132+dan99925@users.noreply.github.com>
2020-07-27 21:35:33 +0300

    Home work for HILLEL

--- HomeWork/HW20.py
+++ HomeWork/HW20.py
@@ -1,12 +1,17 @@
 from string import ascii_uppercase
 from string import digits
 
-str_map = digits + ascii_uppercase
 base = int(input("введите число системы от 2 до 36"))
 value = int(input("введите число"))
-result = ''
-while value > 0:
-    result = str_map[value % base] + result
-    value = value // base
 
-print(result)
+
+def convert(x_base, y_value):
+    str_map = digits + ascii_uppercase
+    result = ''
+    while y_value > 0:
+        result = str_map[y_value % x_base] + result
+        y_value = y_value // x_base
+    return result
+
+
+print(convert(base, value))
